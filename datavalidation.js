ac5d2aa34c1064c93c9572cdbf4a5814a8411d37
Mohammadreza Namjoo <55615117+mohammadrezanamjoo@users.noreply.github.com>
2024-02-18 11:24:04 +0330

    Create datavalidation.js

---
+++ datavalidation.js
@@ -0,0 +1,12 @@
+// Example data validation utility using Joi
+const Joi = require('joi');
+
+function validateData(data, schema) {
+    const { error, value } = schema.validate(data);
+    if (error) {
+        throw new Error(`Validation error: ${error.details.map(detail => detail.message).join(', ')}`);
+    }
+    return value;
+}
+
+module.exports = { validateData };
