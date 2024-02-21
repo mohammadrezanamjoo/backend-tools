970c24cae7c92bd6ee07f9932e3814051e130397
Mohammadreza Namjoo <55615117+mohammadrezanamjoo@users.noreply.github.com>
2024-02-21 14:40:56 +0330

    Create crudHelper.js

---
+++ crudHelper.js
@@ -0,0 +1,29 @@
+// Example CRUD operations helper using MongoDB and Mongoose
+const mongoose = require('mongoose');
+
+// Define a mongoose schema and model
+const UserSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number
+});
+
+const User = mongoose.model('User', UserSchema);
+
+async function createUser(data) {
+    return await User.create(data);
+}
+
+async function getUserById(id) {
+    return await User.findById(id);
+}
+
+async function updateUser(id, data) {
+    return await User.findByIdAndUpdate(id, data, { new: true });
+}
+
+async function deleteUser(id) {
+    return await User.findByIdAndDelete(id);
+}
+
+module.exports = { createUser, getUserById, updateUser, deleteUser };
