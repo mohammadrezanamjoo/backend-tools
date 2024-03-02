9dd4b033ad574ecf3839faa9422ecb7758a90080
Mohammadreza Namjoo <55615117+mohammadrezanamjoo@users.noreply.github.com>
2024-03-02 15:51:26 +0330

    Update ratelimit_Middleware.js

--- ratelimit_Middleware.js
+++ ratelimit_Middleware.js
@@ -4,7 +4,7 @@ const rateLimit = require( 'express-rate-limit');
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, 
     max: 100, 
-    message: 'Too many requests, please try again later '
+    message: ' Too many requests, please try again later '
 });
 
 module.exports = { limiter };
