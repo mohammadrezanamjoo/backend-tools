2bbf0627b28e521947cc5681a108540255015cff
Mohammadreza Namjoo <55615117+mohammadrezanamjoo@users.noreply.github.com>
2024-03-13 10:07:48 +0330

    Update loadbalancer.js

--- loadbalancer.js
+++ loadbalancer.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const httpProxy = require('http-proxy');
 
-// List of servers to balance between
 const servers = [
     { host: 'localhost', port: 3001 },
     { host: 'localhost', port: 3002 },
