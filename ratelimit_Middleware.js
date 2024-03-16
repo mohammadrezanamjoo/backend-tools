08557227906c7932626973873661b91e86ed9458
Mohammadreza Namjoo <55615117+mohammadrezanamjoo@users.noreply.github.com>
2024-03-16 10:57:45 +0330

    Update ratelimit_Middleware.js

--- ratelimit_Middleware.js
+++ ratelimit_Middleware.js
@@ -5,6 +5,6 @@ const limiter = rateLimit ( {
     windowMs: 15 * 60 * 1000 , 
     max: 100 , 
     message: ' Too many requests, please try again later '
-});
+}) ;
 
 module.exports = { limiter };
