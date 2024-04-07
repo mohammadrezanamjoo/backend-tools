1f0b35617d9edc1b1c715caff8cf4958bf83f901
Mohammadreza Namjoo <55615117+mohammadrezanamjoo@users.noreply.github.com>
2024-04-07 11:02:24 +0330

    Create captcha.js

---
+++ captcha.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const svgCaptcha = require('svg-captcha');
+
+const app = express();
+const port = 3000;
+
+app.get('/captcha', (req, res) => {
+  const captcha = svgCaptcha.create();
+  res.type('svg');
+  res.status(200).send(captcha.data);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
