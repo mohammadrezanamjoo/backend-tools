d2f994697cb1efbc6da023b130737bda38b1e4c4
Mohammadreza Namjoo <55615117+mohammadrezanamjoo@users.noreply.github.com>
2024-04-13 16:24:02 +0330

    Update cpuramoptimize.js

--- cpuramoptimize.js
+++ cpuramoptimize.js
@@ -5,7 +5,6 @@ const os = require('os');
 if (cluster.isMaster) {
   const numCPUs = os.cpus().length;
 
-  // Fork workers for each CPU
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
