35d11e4f2abf401d71efd0d1c659c51cadb3fb16
Mohammadreza Namjoo <55615117+mohammadrezanamjoo@users.noreply.github.com>
2024-05-08 12:27:40 +0330

    Update loadbalancer.js

--- loadbalancer.js
+++ loadbalancer.js
@@ -4,7 +4,7 @@ const httpProxy = require('http-proxy');
 const servers = [
     { host: 'localhost', port: 3001 },
     { host: 'localhost', port: 3002 },
-    // Add more servers as neede
+    // Add more servers as need
 ];
 
 const proxy = httpProxy.createProxyServer({});
