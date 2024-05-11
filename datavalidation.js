41ac1ae543b0e2bfcd8d9b65c24e88514d9cefda
Mohammadreza Namjoo <55615117+mohammadrezanamjoo@users.noreply.github.com>
2024-05-11 18:25:06 +0330

    Update datavalidation.js

--- datavalidation.js
+++ datavalidation.js
@@ -1,7 +1,7 @@
 
 const Joi = require ('joi');
 
-function validateData(data, schema) {
+function validateData (data, schema) {
     const { error, value } =  schema.validate(data);
     if (error) {
         throw new  Error(`Validation error:  ${error.details.map(detail =>  detail.message).join(', ')}`);
