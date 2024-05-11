40a88993b9419983816a20b917c4be4424667faf
Mohammadreza Namjoo <55615117+mohammadrezanamjoo@users.noreply.github.com>
2024-05-11 18:25:16 +0330

    Update queryBuilderHelper.js

--- queryBuilderHelper.js
+++ queryBuilderHelper.js
@@ -1,7 +1,7 @@
 const { Op } = require('sequelize');
 const { User } = require('../models');
 
-async function getUsersByAgeRange( minAge , maxAge ) {
+async function  getUsersByAgeRange( minAge , maxAge ) {
     return await User.findAll({
         where: {
             age: {
