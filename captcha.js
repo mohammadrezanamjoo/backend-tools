036f2384dfacde1027e3d599c6f150b061b99511
Mohammadreza Namjoo <55615117+mohammadrezanamjoo@users.noreply.github.com>
2024-08-26 18:33:10 +0330

    Update captcha.js

--- captcha.js
+++ captcha.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get('/captcha', (req, res) => {
+  
   const captcha = svgCaptcha.create();
   res.type('svg');
   res.status(200).send(captcha.data);
